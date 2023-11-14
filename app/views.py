821065428b51356db8d65306b82216cf771e9f05
Vladimir Bykov <Voviihb@gmail.com>
2023-11-14 17:40:11 +0300

    add paginator

--- app/views.py
+++ app/views.py
@@ -16,28 +16,41 @@ def prepare_tags():
     } for tag in t]
     return res
 
+
 TAGS = prepare_tags()
 print("PREPARE")
 
+
 def paginate(objects, page, per_page=10):
     paginator = Paginator(objects, per_page)
-    return paginator.page(page).object_list
+    total_pages = paginator.num_pages
+    return paginator.page(page).object_list, total_pages
 
 
 # page -> request
 
 
 # Create your views here.
+def not_found(request):
+    return render(request, '404.html')
+
+
 def index(request):
     # TAGS = prepare_tags()
     QUESTIONS = Question.objects.sort_new()
     page = request.GET.get('page', 1)
+
     try:
-        return render(request, 'index.html', {'questions': paginate(QUESTIONS, page), 'tags': TAGS})
+        if int(page) < 1:
+            page = 1
+    except ValueError:
+        page = 1
+    try:
+        data, total_pages = paginate(QUESTIONS, page)
+        return render(request, 'index.html', {'questions': data,
+                                              'tags': TAGS, 'page_num': page, 'total_pages': int(total_pages)})
     except EmptyPage:
-        return render(request, 'index.html', {'questions': paginate(QUESTIONS, 1), 'tags': TAGS})
-    except PageNotAnInteger:
-        return render(request, 'index.html', {'questions': paginate(QUESTIONS, 1), 'tags': TAGS})
+        return not_found(request)
 
 
 def question(request, question_id):
@@ -45,17 +58,24 @@ def question(request, question_id):
     QUESTIONS = Question.objects.all()
     page = request.GET.get('page', 1)
     try:
+        if int(page) < 1:
+            page = 1
+    except ValueError:
+        page = 1
+
+    try:
         item = QUESTIONS[question_id - 1]
     except IndexError:
         item = QUESTIONS[0]
     answers = Answer.objects.get_answers(question_id)
 
     try:
-        return render(request, 'question.html', {'question': item, 'answers': paginate(answers, page), 'tags': TAGS})
+        data, total_pages = paginate(answers, page)
+        return render(request, 'question.html',
+                      {'question': item, 'answers': data, 'tags': TAGS, 'page_num': page,
+                       'total_pages': int(total_pages)})
     except EmptyPage:
-        return render(request, 'question.html', {'question': item, 'answers': paginate(answers, 1), 'tags': TAGS})
-    except PageNotAnInteger:
-        return render(request, 'question.html', {'question': item, 'answers': paginate(answers, 1), 'tags': TAGS})
+        return not_found(request)
 
 
 def ask(request):
@@ -83,21 +103,33 @@ def hot(request):
     QUESTIONS = Question.objects.sort_hot()
     page = request.GET.get('page', 1)
     try:
-        return render(request, 'hot.html', {'questions': paginate(QUESTIONS, page), 'tags': TAGS})
+        if int(page) < 1:
+            page = 1
+    except ValueError:
+        page = 1
+
+    try:
+        data, total_pages = paginate(QUESTIONS, page)
+        return render(request, 'hot.html', {'questions': data,
+                                            'tags': TAGS, 'page_num': page, 'total_pages': int(total_pages)})
     except EmptyPage:
-        return render(request, 'hot.html', {'questions': paginate(QUESTIONS, 1), 'tags': TAGS})
-    except PageNotAnInteger:
-        return render(request, 'hot.html', {'questions': paginate(QUESTIONS, 1), 'tags': TAGS})
+        return not_found(request)
 
 
 def tag(request, tag_name):
     # TAGS = prepare_tags()
     page = request.GET.get('page', 1)
     res = Tag.objects.get_questions(tag_name)
+    try:
+        if int(page) < 1:
+            page = 1
+    except ValueError:
+        page = 1
 
     try:
-        return render(request, 'tag.html', {'tag': tag_name, 'questions': paginate(res, page), 'tags': TAGS})
+        data, total_pages = paginate(res, page)
+        return render(request, 'tag.html',
+                      {'tag': tag_name, 'questions': data, 'tags': TAGS, 'page_num': page,
+                       'total_pages': int(total_pages)})
     except EmptyPage:
-        return render(request, 'tag.html', {'questions': paginate(res, 1), 'tags': TAGS})
-    except PageNotAnInteger:
-        return render(request, 'tag.html', {'questions': paginate(res, 1), 'tags': TAGS})
+        return not_found(request)
--- askme_bykov/urls.py
+++ askme_bykov/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('hot', views.hot, name="hot"),
     path('tag/<str:tag_name>', views.tag, name="tag"),
     path('admin/', admin.site.urls),
+    path('404', views.not_found, name="not_found")
 ]
